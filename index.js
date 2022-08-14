47c3ebb6b59ba4b9a7a3d04b6e7df9bea448d449
Zac Smith <zrs3141592@gmail.com>
2022-08-14 14:07:19 -0600

    👍 IMPROVE: prompt with when

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 import inquirer from 'inquirer'
 
-// prompt with password
-
-const requireLetterAndNumber = (value) => {
-  if (/\w/.test(value) && /\d/.test(value)) {
-    return true
-  }
-  return 'password needs to have at least one letter and one number'
-}
-
 inquirer
   .prompt([
     {
-      type: 'password',
-      name: 'enter a masked password',
-      name2: 'enter a masked password to confirm',
-      validate: requireLetterAndNumber,
+      type: 'confirm',
+      name: 'pizzaFav',
+      message: 'Is pizza your favorite food?',
     },
   ])
-  .then((answers) => console.info(`your answers are${JSON.stringify(answers)}`))
+  .then((answers) => {
+    if (!answers.pizzaFav) {
+      inquirer.prompt({
+        type: 'input',
+        name: 'why',
+        message: 'Oh! What could be more delicious than pizza?',
+      })
+    }
+  })
+
+// prompt with when
+// inquirer
+//   .prompt([
+//     {
+//       type: 'confirm',
+//       name: 'pizzaFav',
+//       message: 'Is pizza your favorite food?',
+//     },
+//     {
+//       type: 'input',
+//       name: 'why',
+//       message: 'Oh! What could be more delicious than pizza?',
+//       when: (answers) => answers.pizzaFav === false,
+//     },
+//   ])
+//   .then((answers) => {
+//     console.info('Your answer are :', JSON.stringify(answers))
+//   })
+
+// prompt with password
+
+// const requireLetterAndNumber = (value) => {
+//   if (/\w/.test(value) && /\d/.test(value)) {
+//     return true
+//   }
+//   return 'password needs to have at least one letter and one number'
+// }
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'password',
+//       name: 'enter a masked password',
+//       name2: 'enter a masked password to confirm',
+//       validate: requireLetterAndNumber,
+//     },
+//   ])
+//   .then((answers) => console.info(`your answers are${JSON.stringify(answers)}`))
 
 // prompt with editor
 // inquirer
