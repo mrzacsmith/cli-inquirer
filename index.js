356c7472ff8872c7a5098ae7cf0f7158e34c358b
Zac Smith <zrs3141592@gmail.com>
2022-08-14 11:28:56 -0600

    👍 IMPROVE: prompt list

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ import inquirer from 'inquirer'
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your project?',
-      default: 'My Project',
+      type: 'list',
+      name: 'contactSupport',
+      message: 'Which contact would you like to support?',
+      choices: [
+        'by email',
+        'by phone',
+        {
+          name: 'by fax',
+          disabled: 'not supported',
+        },
+        new inquirer.Separator(),
+        'have our agent contact you',
+      ],
     },
   ])
-  .then((answers) => {
-    console.info('Your name is ', JSON.stringify(answers))
-  })
+  .then((answers) =>
+    console.info(`your answers are ${JSON.stringify(answers)}`)
+  )
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is the name of your project?',
+//       default: 'My Project',
+//     },
+//   ])
+//   .then((answers) => {
+//     console.info('Your name is ', JSON.stringify(answers))
+//   })
