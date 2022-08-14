51de77fc7f890b6768144dea687469c74168ed51
Zac Smith <zrs3141592@gmail.com>
2022-08-14 11:35:14 -0600

    👍 IMPROVE: prompt expanded

--- index.js
+++ index.js
@@ -1,27 +1,71 @@
 import inquirer from 'inquirer'
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'contactSupport',
-      message: 'Which contact would you like to support?',
-      choices: [
-        'by email',
-        'by phone',
-        {
-          name: 'by fax',
-          disabled: 'not supported',
-        },
-        new inquirer.Separator(),
-        'have our agent contact you',
-      ],
-    },
-  ])
-  .then((answers) =>
-    console.info(`your answers are ${JSON.stringify(answers)}`)
-  )
+// prompt with expanded options
+// inquirer
+//   .prompt([
+//     {
+//       type: 'expand',
+//       name: 'contactSupport',
+//       message: 'What is your contact support?',
+//       default: 'by chat',
+//       choices: [
+//         {
+//           key: 'e',
+//           name: 'by email',
+//           value: 'email',
+//         },
+//         {
+//           key: 'p',
+//           name: 'by phone',
+//           value: 'phone',
+//         },
+//         {
+//           key: 'a',
+//           name: 'by agent',
+//           value: 'agent',
+//         },
+//       ],
+//     },
+//   ])
+//   .then((answers) => console.info(`your answers: ${JSON.stringify(answers)}`))
 
+//prompt using rawlist
+// inquirer
+//   .prompt([
+//     {
+//       type: 'rawlist',
+//       name: 'contactSupport',
+//       message: 'What is your contact support?',
+//       default: 'by chat',
+//       choices: ['by email', 'by phone', 'have our agents call you'],
+//     },
+//   ])
+//   .then((answers) => console.info(`your answers: ${JSON.stringify(answers)}`))
+
+// prompt using list
+// inquirer
+//   .prompt([
+//     {
+//       type: 'list',
+//       name: 'contactSupport',
+//       message: 'Which contact would you like to support?',
+//       choices: [
+//         'by email',
+//         'by phone',
+//         {
+//           name: 'by fax',
+//           disabled: 'not supported',
+//         },
+//         new inquirer.Separator(),
+//         'have our agent contact you',
+//       ],
+//     },
+//   ])
+//   .then((answers) =>
+//     console.info(`your answers are ${JSON.stringify(answers)}`)
+//   )
+
+// simple prompt
 // inquirer
 //   .prompt([
 //     {
